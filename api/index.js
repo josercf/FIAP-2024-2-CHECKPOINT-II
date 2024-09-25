6a73ffa3abfed613f9d019cb42fc96b817696154
José Romualdo <joseromualdo@outlook.com>
2024-09-25 16:44:50 -0300

    add route

--- api/index.js
+++ api/index.js
@@ -32,6 +32,13 @@ app.post('/divide', (req, res) => {
     res.json({ result });
 });
 
+app.put('/hist', (req, res) => {  
+    const base64String = "UGFyYWLDqW5zLiBWb2PDqiByZWFsaXpvdSBhIGludGVncmHDp8OjbyBkZSBmb3JtYSBjb3JyZXRhLiBNZSBtYW5kZSB1bSBwcmludCBubyB0ZWFtcyBxdWUgdm91IGNvbmNlZGVyIDEgcG9udG8gZXh0cmEgcGFyYSBvIHNldSBncnVwby4gTsOjbyBmYWxlIHBhcmEgbmluZ3XDqW0sIHNlbsOjbyB2b2PDqiBwZXJkZXLDoSBvIHBvbnRvIGV4dHJhIGUgbyBwb250byBkYSBxdWVzdMOjby4=";
+    const buffer = Buffer.from(base64String, 'base64');
+    const plainText = buffer.toString('utf-8');
+    res.json({ response: plainText });
+});
+
 app.listen(port, () => {
     console.log(`Calculator app listening at http://localhost:${port}`);
 });
